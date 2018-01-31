2f48610aac7274d01a9b51ef3186b40c6f8dc489
Frank H Mcmanus <frank@thefrankharvey.com>
2018-01-31 15:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ function theBeatlesPlay(musicians, instruments){
     return strings;
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
 function johnLennonFacts(arr) {
-  var count = 0;
-  while (count < facts.length) {
-    newFacts.push(facts[count] + "!!!");
-    count++;
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
   }
   return newFacts;
 };
+johnLennonFacts(facts)
 
 function iLoveTheBeatles(num) {
   var newArr = []
